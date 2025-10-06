b4df1a82aa258eb396c7db3d32d2852b71c1cd4c
Santygm04 <santiagogilmina2@gmail.com>
2025-10-06 15:16:10 -0300

    cambios en el banner del hero #11

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import "../../components/Hero/Hero.css";
 import primavera from "/assets/primavera.jpg"
 import primavera1 from "/assets/primavera-1.png"
-import primavera2 from "/assets/primavera.png"
+import primavera2 from "/assets/primavera-2.png"
 
 const RAW_WA = import.meta.env.VITE_SELLER_PHONE || "+5493854112412";
 const SELLER_WA = String(RAW_WA).replace(/\D/g, "");
